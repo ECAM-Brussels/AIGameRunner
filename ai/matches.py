731caf4fa740c3fd4a5df366b3ac96f9dd96eeda
Quentin Lurkin <qlurkin@gmail.com>
2019-04-02 23:30:32 +0200

    CORS

--- ai/matches.py
+++ ai/matches.py
@@ -1,12 +1,27 @@
 import cherrypy
+import sys
 
 class Server:
     @cherrypy.expose
     @cherrypy.tools.json_in()
     @cherrypy.tools.json_out()
     def move(self):
+        # Deal with CORS
+        cherrypy.response.headers['Access-Control-Allow-Origin'] = '*'
+        cherrypy.response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+        cherrypy.response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, X-Requested-With'
+        if cherrypy.request.method == "OPTIONS":
+            return ''
+        
         body = cherrypy.request.json
         print(body)
         return {"move": 1}
 
-cherrypy.quickstart(Server())
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        port=int(sys.argv[1])
+    else:
+        port=8080
+
+    cherrypy.config.update({'server.socket_port': port})
+    cherrypy.quickstart(Server())

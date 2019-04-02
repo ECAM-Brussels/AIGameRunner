47c75c95fc4a23bf37022733e7673182483ebfc5
Quentin Lurkin <qlurkin@gmail.com>
2019-04-02 23:33:55 +0200

    Start Match

--- public/js/matchListTemplate.js
+++ public/js/matchListTemplate.js
@@ -1,5 +1,7 @@
 import {html} from '/modules/lit-html/lit-html.js'
 import {matchList} from '/js/matchList.js'
+import {store} from '/js/store.js'
+import {runMatch} from '/js/match.js'
 
 export const matchListTemplate = participants => html`
 	<ul id="matchList" class="mdc-list mdc-elevation--z4">
@@ -8,7 +10,12 @@ export const matchListTemplate = participants => html`
                 <span class="mdc-list-item__text">
                     ${match.get(0)}<br>
 					${match.get(1)}
-				</span>
+                </span>
+                <span class="mdc-list-item__meta material-icons" style="cursor: pointer" @click="${() => {
+                    const state = store.getState()
+                    const participants = state.get('participants')
+                    store.dispatch(runMatch(participants.get(match.get(0)), participants.get(match.get(1))))
+                }}">play_arrow</span>
             </li>
         `).toArray()}
     </ul>

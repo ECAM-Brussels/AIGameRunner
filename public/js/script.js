ff3862d4b0433f989f6b812a10d74adf0034d27b
Quentin Lurkin <qlurkin@gmail.com>
2019-04-04 02:35:28 +0200

    Load Participants from LocalStorage

--- public/js/script.js
+++ public/js/script.js
@@ -8,6 +8,17 @@ document.addEventListener("DOMContentLoaded", event => {
 
 	store.subscribe(() => {render(mainTemplate(store.getState()), document.getElementById('slot'))})
 
-	store.dispatch(addParticipant('LUR', 'localhost', '8080'))
-	store.dispatch(addParticipant('CBF', 'localhost', '8081'))
+	const state = JSON.parse(localStorage.getItem('state'))
+	console.log(state)
+	if(state && state.participants) {
+		Object.keys(state.participants).map(name => {
+			const participant = state.participants[name]
+			store.dispatch(addParticipant(participant.name, participant.ip, participant.port))
+		})
+	}
+
+	store.dispatch({type: "FIRST_RENDER"})
+
+	//store.dispatch(addParticipant('Player 1', 'localhost', '8080'))
+	//store.dispatch(addParticipant('Player 2', 'localhost', '8081'))
 })

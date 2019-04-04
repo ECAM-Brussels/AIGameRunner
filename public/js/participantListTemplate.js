108201b4aaab1511cd84d713e3955394828ae3ea
Quentin Lurkin <qlurkin@gmail.com>
2019-04-04 02:34:45 +0200

    Add remove participant UI

--- public/js/participantListTemplate.js
+++ public/js/participantListTemplate.js
@@ -1,4 +1,6 @@
 import {html} from '/modules/lit-html/lit-html.js'
+import {removeParticipant} from '/js/participants.js'
+import {store} from '/js/store.js'
 
 export const participantListTemplate = participants => html`
 	<ul id="participantList" class="mdc-list mdc-list--two-line mdc-elevation--z4">
@@ -8,6 +10,9 @@ export const participantListTemplate = participants => html`
 					<span class="mdc-list-item__primary-text">${participant.get('name')}</span>
 					<span class="mdc-list-item__secondary-text">${participant.get('ip')}:${participant.get('port')}</span>
 				</span>
+				<span class="mdc-list-item__meta material-icons" style="cursor: pointer" @click="${() => {
+                    store.dispatch(removeParticipant(participant.get('name')))
+                }}">cancel</span>
 			</li>
 		`).valueSeq().toArray()}
 	</ul>

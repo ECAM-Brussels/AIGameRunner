5697461735cc42e9e0ad72b6ce4ae0551cfb9a32
Quentin Lurkin <qlurkin@gmail.com>
2020-03-22 13:12:37 +0100

    Querying participants from server

--- public/js/script.js
+++ public/js/script.js
@@ -13,12 +13,31 @@ document.addEventListener("DOMContentLoaded", event => {
 	if(state && state.participants) {
 		Object.keys(state.participants).map(name => {
 			const participant = state.participants[name]
-			store.dispatch(addParticipant(participant.name, participant.ip, participant.port))
+			store.dispatch(addParticipant(participant.name, participant.ip, participant.port, participant.matricules))
 		})
 	}
 
 	store.dispatch({type: "FIRST_RENDER"})
 
-	//store.dispatch(addParticipant('Player 1', 'localhost', '8080'))
-	//store.dispatch(addParticipant('Player 2', 'localhost', '8081'))
+	//store.dispatch(addParticipant('Player 1', 'localhost', '8080', ['lur']))
+	//store.dispatch(addParticipant('Player 2', 'localhost', '8081', ['lrg']))
+
+	setInterval(() => {
+		fetch('/clients')
+		.then(response => response.json())
+		.then(data => {
+			const participants = store.getState().get('participants').toJS()
+			data.forEach(client => {
+				if(participants[client.name] === undefined) {
+					store.dispatch(addParticipant(client.name, client.ip, String(client.port), client.matricules))
+				}
+			})
+
+			Object.keys(participants).forEach(name => {
+				if(!data.map(client => client.name).includes(name)) {
+					store.dispatch(removeParticipant(name))
+				}
+			})
+		})
+	}, 5000)
 })
